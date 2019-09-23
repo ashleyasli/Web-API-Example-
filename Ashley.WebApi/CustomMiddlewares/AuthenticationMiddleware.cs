2388d2a32151c5c82e73ca000a77787069f1d331
Ashley Saylik <ashley.saylik@tetratech.com>
2019-09-23 14:49:41 -0500

    Basic Authentication Middleware

--- Ashley.WebApi/CustomMiddlewares/AuthenticationMiddleware.cs
+++ Ashley.WebApi/CustomMiddlewares/AuthenticationMiddleware.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore.Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Ashley.WebApi.CustomMiddlewares
@@ -15,7 +18,28 @@ namespace Ashley.WebApi.CustomMiddlewares
         }
         public async Task Invoke(HttpContext context)
         {
-            var autHeader = context.Request.Headers["Authorization"];
+            string authHeader = context.Request.Headers["Authorization"];
+            //basic ashley:12345
+            if(authHeader!=null && authHeader.StartsWith("basic", StringComparison.OrdinalIgnoreCase))
+            {
+                var token = authHeader.Substring(6).Trim();
+                var credentialString = Encoding.UTF8.GetString(Convert.FromBase64String(token));
+                var credentials = credentialString.Split(':');
+                if (credentials[0] == "ashley" && credentials[1] == "12345")
+                {
+                    var claims = new[]
+                    {
+                        new Claim("name", credentials[0]),
+                        new Claim(ClaimTypes.Role, "Admin")                       
+                    };
+                    var identity = new ClaimsIdentity(claims, "Basic");
+                    context.User = new ClaimsPrincipal(identity);
+                }
+            }
+            else
+            {
+                context.Response.StatusCode = 401;
+            }
 
             await _next(context);
         }
